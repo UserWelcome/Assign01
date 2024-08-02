a8a94474b115b10b18a416e23aa3ecc337e520ca
UserWelcome <135332037+UserWelcome@users.noreply.github.com>
2024-08-02 13:04:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,16 +16,13 @@ app.use('/api/auth', authRoutes);
 app.use('/api/auth', userRoutes);
 
 // MongoDB Connection
-const MONGO_URI = 'mongodb://127.0.0.1:27017/user_mngmt'; // Update with your actual MongoDB URI
-
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const MONGO_URI = 'mongodb://127.0.0.1:27017/user_mngmt'; 
+mongoose.connect(MONGO_URI)
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+      console.log(`connected to db`);
   })
   .catch(err => {
     console.error('Failed to connect to MongoDB', err);
   });
-//
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
